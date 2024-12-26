a126156d550d447ba2f6fa4b08826b100a3cd17e
Dhia Nabli <bytejutsuproject@gmail.com>
2024-12-26 12:29:26 +0100

    initialized a database connection with supabase

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,23 @@
 import streamlit as st
-from st_supabase_connection import SupabaseConnection
+from supabase import create_client, Client
 
 # Initialize connection.
-conn = st.connection("supabase",type=SupabaseConnection)
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = init_connection()
 
 # Perform query.
-rows = conn.query("*", table="mytable", ttl="10m").execute()
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query():
+    return supabase.table("mytable").select("*").execute()
+
+rows = run_query()
 
 # Print results.
 for row in rows.data:
